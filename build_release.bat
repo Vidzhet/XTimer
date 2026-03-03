e2c50986766d0a261130c2baaa9df89e2b584fe6
Vidzhet <vidzhetcontact@gmail.com>
2026-03-03 16:44:46 +0100

    add release build script

---
+++ build_release.bat
@@ -0,0 +1,13 @@
+set BUILD_DIR=build
+
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+cmake -S .. -B . -DCMAKE_PREFIX_PATH="C:\Qt\6.10.1\msvc2022_64\lib\cmake"
+
+cmake --build . --target XTimerAPI --config Release
+cmake --build . --target XTimer --config Release
+
+C:\Qt\6.10.1\msvc2022_64\bin\windeployqt.exe XTimer\Release\XTimer.exe --release
+
+pause
